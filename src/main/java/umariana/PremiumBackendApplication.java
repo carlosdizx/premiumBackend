4d0265e487cf0bb2c00b0092e6e18521b43538b2
Carlos Diaz <carlodiaz@umariana.edu.co>
2021-03-18 15:41:44 -0500

    Creamos el proyecto, con la configuracion basica de spring y lista para subirse al repositorio

---
+++ src/main/java/umariana/PremiumBackendApplication.java
@@ -0,0 +1,13 @@
+package umariana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremiumBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PremiumBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/umariana/PremiumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package umariana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremiumBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
