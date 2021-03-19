6bd9f8fe52ee40528cf56944d3ee7db74712852f
Carlos Diaz <carlodiaz@umariana.edu.co>
2021-03-19 11:53:43 -0500

    Se agrego el metodo para actulizar datos

--- src/main/java/umariana/controllers/EmpresaRest.java
+++ src/main/java/umariana/controllers/EmpresaRest.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import umariana.models.entity.Empresa;
 import umariana.models.service.api.EmpresaService;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -48,9 +49,13 @@ public class EmpresaRest
 
     }
 
-    @PostMapping("/")
+    @PostMapping("/all")
     public ResponseEntity<Map<String,Object>>save(@RequestBody Empresa pEmpresa){
         final Map<String, Object>response = new HashMap<>();
+        if (pEmpresa == null){
+            response.put("mensaje","Datos nulos o vacios");
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
         try{
             final Empresa empresa = service.save(pEmpresa);
             if (empresa == null){
@@ -63,6 +68,35 @@ public class EmpresaRest
         }
         catch (DataAccessException e){
             response.put("mensaje","No se ha logrado realizar la insercion en la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage() ));
+            return new ResponseEntity<>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@Valid @RequestBody Empresa pEmpresa, @PathVariable Long id )
+    {
+        final Map<String,Object> response =new HashMap<>();
+        try{
+            final Empresa empresa = service.findByID(id);
+            if (empresa==null){
+                response.put("mensaje","la empresa con el id: ".concat(id.toString()).concat(" no existe! ")
+                        .concat(",no se puede editar"));
+                return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
+            }
+            empresa.setNit(empresa.getNit() );
+            empresa.setNombre(empresa.getNombre() );
+            empresa.setSector( pEmpresa.getSector() );
+            empresa.setTelefono( pEmpresa.getTelefono() );
+            empresa.setCorreo( pEmpresa.getCorreo() );
+            empresa.setCelular( pEmpresa.getCelular() );
+
+            response.put("empresa",service.save( empresa ));
+            response.put("mensaje","La empresa '"+empresa.getNombre()+"' ha sido actualizada");
+            return new ResponseEntity<>(response,HttpStatus.CREATED);
+        }
+        catch (DataAccessException e){
+            response.put("mensaje","No se ha logrado realizar la actualizacion de datos de la empresa");
             response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage() ));
             return new ResponseEntity<>(response,HttpStatus.INTERNAL_SERVER_ERROR);
         }
